91f9dcc3d4df0866ea1c41eb9f1aa3c68b4bb088
skhiri jihen <jihen.skhiri@gmail.com>
2024-09-18 18:52:31 +0200

    CORREC 1 commit

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -4,7 +4,7 @@ import './Collapse.css'
 
 export default function Collapse(props) {
 	const [toggle, setToggle] = useState(false); // je definis le state du toggle (et false par défaut)
-	const [heightEl, setHeightEl] = useState(); // je definis le state de la hauteur du collapse
+	const [heightEl, setHeightEl] = useState(null); // je definis le state de la hauteur du collapse
 
 	const toggleState = () => {
 		//je définis la fonction toggleState qui modifie la valeur toggle au clic
@@ -15,12 +15,12 @@ export default function Collapse(props) {
 
 	useEffect(() => {
 		setHeightEl(`${refHeight.current.scrollHeight}px`); //useEffect s'éxécute au montage du composant, dans ce cas, il définit la hauteur du collapse déplié lors de sa première ouverture et la conserve dans refHeight
-	}, []);
+	}, [props.aboutText]);
 
 	return (
 		// affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
 		<div className={`collapse ${props.aboutStyle}`}>
-			<div onClick={toggleState} className="collapse__visible">
+			<div onClick={toggleState} className="collapse__visible" >
 				<h2 className="collapse-title">{props.aboutTitle}</h2>
 				<img
 					className={toggle ? "chevron rotated" : "chevron"}
@@ -31,9 +31,9 @@ export default function Collapse(props) {
 			<div
 				ref={refHeight}
 				className={toggle ? "collapse__toggle animated" : "collapse__toggle"}
-				style={{ height: toggle ? `${heightEl}` : "0px" }}
+				style={{ maxHeight: toggle ? `${heightEl}` : "0px" }}
 			>
-				<p aria-hidden={toggle ? "true" : "false"}>{props.aboutText}</p>
+				<p aria-hidden={toggle ? "false" : "true"}>{props.aboutText}</p>
 			</div>
 		</div>
 	);
